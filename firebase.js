c7b955b5fee44a20e03b513a77790b94725f4e0e
Felipe Stuart <felipe5stuart@gmail.com>
2023-06-26 00:32:19 -0300

    wip

--- firebase.js
+++ firebase.js
@@ -4,16 +4,6 @@ const { initializeApp, applicationDefault, cert } = require('firebase-admin/app'
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 const serviceAccount = require('./credential.json');
 
-/* const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID ,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-}; */
-
 initializeApp({
   credential: cert(serviceAccount)
 });
